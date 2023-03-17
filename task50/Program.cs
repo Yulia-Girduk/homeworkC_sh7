e79d8a7e1a6a86a51c378f34e821bfba91e22eae
Nick Gir <mbiko@ya.ru>
2023-03-17 18:26:18 +0300

    add task50

---
+++ task50/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// Выводим сообщения для пользователя и возвращаем введенные данные пользователем
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Выводим массив на экран
+void PrintArray(int[,] tableArray)
+{
+    Console.WriteLine();
+    for (int i = 0; i < tableArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tableArray.GetLength(1); j++)
+        {
+            Console.Write($"{tableArray[i, j]}      ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Генерируем (задаем) массив
+int[,] GenerateArray(int numberRow, int numberColumn, int numberStart, int numberEnd)
+{
+    Random random = new Random();
+    int[,] tableArray = new int[numberRow, numberColumn];
+    for (int i = 0; i < tableArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tableArray.GetLength(1); j++)
+        {
+            tableArray[i, j] = random.Next(numberStart, numberEnd+1);
+        }
+    }
+    return tableArray;
+}
+
+// Проверяем есть ли заданое число в массиве или нет
+
+
+int numberRow = Prompt("Введите значение количества строк массива: ");
+int numberColumn = Prompt("Введите значение количества столбцов массива: ");
+int numberStart = Prompt("Введите начальное значение для заполнения массива: ");
+int numberEnd = Prompt("Введите конечное значение для заполнения массива: ");
+
+int[,] tableArray = GenerateArray(numberRow,numberColumn,numberStart,numberEnd);
+PrintArray(tableArray);
+
